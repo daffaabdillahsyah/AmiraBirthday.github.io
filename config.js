7da2e9b4b9bf82252edce6e8f24ae8d00a80f4f9
daffaabdillahsyah <116296645+daffaabdillahsyah@users.noreply.github.com>
2023-01-23 23:17:40 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,32 @@
+var config = {
+
+    texts: [
+        "HAPPY BIRTHDAY!!!",
+        "Peri kecilku",
+        "Semoga panjang umur yaa",
+        "Semoga labilnya berkurang",
+        "Semoga makin sering senyum",
+        "Semoga semua wish kamu terkabul",
+        "Aamiinn",
+        "Jadi 19 tahun ga selamanya buruk koo",
+        "Pokonya apapun yang terjadi",
+        "Aku bakal selalu ada dan dukung kamu terus",
+        "Semangat Peri Kecil ku <3",
+    ],
+
+    imgs: {
+        "Peri kecilku": "./imgs/xiaokeai.png",
+
+    },
+
+    desc: {
+        turn_on: "START",
+        play: "MUSIC ON",
+        bannar_coming: "DROP",
+        balloons_flying: "BALOONS",
+        cake_fadein: "CAKE?",
+        light_candle: "CANDLE?",
+        wish_message: "HAPPY BIRTHDAY!",
+        story: "A MESSAGE FOR YOU",
+    }
+};
